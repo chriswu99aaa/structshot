6155c6139035bf7e819b6f666e8377a5448f2c90
Shuaimin <75012086+chriswu99aaa@users.noreply.github.com>
2023-11-27 21:41:05 +0000

    Update lightning_base.py

--- structshot/lightning_base.py
+++ structshot/lightning_base.py
@@ -264,7 +264,7 @@ class BaseTransformer(pl.LightningModule):
 
 
 class LoggingCallback(pl.Callback):
-    def on_batch_end(self, trainer, pl_module):
+    def on_train_batch_end(self, trainer, pl_module):
         lr_scheduler = trainer.lr_schedulers[0]["scheduler"]
         lrs = {f"lr_group_{i}": lr for i, lr in enumerate(lr_scheduler.get_lr())}
         pl_module.logger.log_metrics(lrs)
