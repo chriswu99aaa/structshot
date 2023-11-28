915867857edc15bfa10b20a11242b84b16f00640
Shuaimin <75012086+chriswu99aaa@users.noreply.github.com>
2023-11-28 14:11:00 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="structshot",
     packages=find_packages(),
     install_requires=[
-        "pytorch-lightning~=1.8.6",
-        "transformers~=4.35.2",
+        "pytorch-lightning~=0.8.5",
+        "transformers~=3.3.1",
         "seqeval",
     ],
 )
